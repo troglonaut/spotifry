0fd43c9055bbba07071520341aaa6d71edf76e1f
Eric <ericscottrichards@gmail.com>
2024-04-18 18:55:17 -0600

    playlist types

--- types/types.ts
+++ types/types.ts
@@ -1,5 +1,34 @@
 import { DefaultSession } from "next-auth";
 
+export interface Album {
+  album_type: "album" | "single" | "compilation";
+  total_tracks: number;
+  available_markets: string[];
+  external_urls: ExternalUrls;
+  href: string;
+  id: string;
+  images: ImageObject[];
+  name: string;
+  release_date: string;
+  release_date_precision: "year" | "month" | "day";
+  restrictions: Restrictions;
+  type: "album";
+  uri: string;
+}
+
+export interface ArtistObject {
+  external_urls: ExternalUrls;
+  followers: Followers;
+  genres: string[];
+  href: string;
+  id: string;
+  images: ImageObject;
+  name: string;
+  popularity: number;
+  type: "artist";
+  uri: string;
+}
+
 export interface AuthUser {
   name: string;
   email: string;
@@ -14,9 +43,43 @@ export interface AuthSession extends Omit<DefaultSession, "user"> {
   user: AuthUser;
 }
 
+export interface CopyrightObject {
+  text: string;
+  type: "C" | "P";
+}
+
+export interface EpisodeObject {
+  audio_preview_url: string | null;
+  description: string;
+  html_description: string;
+  duration_ms: number;
+  explicit: boolean;
+  external_urls: ExternalUrls;
+  href: string;
+  id: string;
+  images: ImageObject[];
+  is_externally_hosted: boolean;
+  is_playable: boolean;
+  languages: string[];
+  name: string;
+  release_date: string;
+  release_date_precision: "year" | "month" | "day";
+  resume_point: ResumePoint;
+  type: "episode";
+  uri: string;
+  restrictions: Restrictions;
+  show: ShowObject;
+}
+
 export interface ExplicitContentFilterConfig {
   filter_enabled: boolean;
   filter_locked: boolean;
+}
+
+export interface ExternalIds {
+  isrc: string;
+  ean: string;
+  upc: string;
 }
 
 export interface ExternalUrls {
@@ -30,8 +93,41 @@ export interface Followers {
 
 export interface ImageObject {
   url: string;
-  height: number;
-  width: number;
+  height: number | null;
+  width: number | null;
+}
+
+export interface Owner {
+  display_name: string | null;
+  external_urls: ExternalUrls;
+  followers: Followers;
+  href: string;
+  id: string;
+  type: "user";
+  uri: string;
+}
+
+export interface Playlist {
+  collaborative: boolean;
+  description: string | null;
+  external_urls: ExternalUrls;
+  followers: Followers;
+  href: string;
+  id: string;
+  images: ImageObject[];
+  name: string;
+  owner: Owner;
+  public: boolean;
+  snapshot_id: string;
+  tracks: TrackObject[];
+  type: "playlist";
+  uri: string;
+}
+
+export interface PlaylistTrackObject {
+  added_at: string;
+  added_by: AuthUser;
+  is_local: boolean;
 }
 
 export interface Profile {
@@ -45,4 +141,74 @@ export interface Profile {
   images: ImageObject[];
   type: "user";
   uri: string;
+}
+
+export interface Restrictions {
+  reason: "market" | "product" | "explicit";
+}
+
+export interface ResumePoint {
+  fully_played: boolean;
+  resume_position_ms: number;
+}
+
+export interface SimplifiedArtistObject {
+  external_urls: ExternalUrls;
+  href: string;
+  id: string;
+  name: string;
+  type: "artist";
+  uri: string;
+}
+
+export interface ShowObject {
+  available_markets: string[];
+  copyrights: CopyrightObject;
+  description: string;
+  html_description: string;
+  explicit: boolean;
+  external_urls: ExternalUrls;
+  href: string;
+  id: string;
+  images: ImageObject[];
+  is_externally_hosted: boolean;
+  languages: string[];
+  media_type: string;
+  name: string;
+  publisher: string;
+  type: "show";
+  uri: string;
+  total_episodes: number;
+}
+
+export interface TrackObject {
+  album: Album;
+  artists: ArtistObject[];
+  available_markets: string[];
+  disc_number: number;
+  duration_ms: number;
+  explicit: boolean;
+  external_ids: ExternalIds;
+  href: string;
+  id: string;
+  is_playable: boolean;
+  linked_from: object;
+  restrictions: Restrictions;
+  name: string;
+  popularity: number;
+  preview_url: string | null;
+  track_number: number;
+  type: string;
+  uri: string;
+  is_local: boolean;
+}
+
+export interface Tracks {
+  href: string;
+  limit: number;
+  next: string | null;
+  offset: number;
+  previous: string | null;
+  total: number;
+  items: PlaylistTrackObject[];
 }
