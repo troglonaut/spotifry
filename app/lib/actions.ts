f0ac527337ac335585532d92c0bd481ed4f22ade
Eric <ericscottrichards@gmail.com>
2024-05-08 18:05:55 -0600

    get my items

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -57,18 +57,52 @@ export const getMyPlaylists = async (
   );
 };
 
-export const getMySavedTracks = async (
+export const getMySavedAlbums = async (
   session: AuthSession,
   limit = 20,
-  market?: string,
+  offset?: number,
+  market?: string
+) => await getMySavedItems(session, "albums", limit, offset, market);
+
+export const getMySavedAudiobooks = async (
+  session: AuthSession,
+  limit = 20,
   offset?: number
+) => await getMySavedItems(session, "audiobooks", limit, offset);
+
+export const getMySavedEpisodes = async (
+  session: AuthSession,
+  limit = 20,
+  offset?: number,
+  market?: string
+) => await getMySavedItems(session, "episodes", limit, offset, market);
+
+export const getMySavedItems = async (
+  session: AuthSession,
+  type: "albums" | "audiobooks" | "episodes" | "shows" | "tracks",
+  limit = 20,
+  offset?: number,
+  market?: string
 ) => {
   const searchParams = new URLSearchParams();
   if (limit) searchParams.set("limit", limit.toString());
   if (offset) searchParams.set("offset", offset.toString());
   if (market) searchParams.set("market", market);
-  return customFetch(`${SPOTIFY_URL_BASE}/v1/me/tracks`, session);
+  return customFetch(`${SPOTIFY_URL_BASE}/v1/me/${type}`, session);
 };
+
+export const getMySavedShows = async (
+  session: AuthSession,
+  limit = 20,
+  offset?: number
+) => await getMySavedItems(session, "shows", limit, offset);
+
+export const getMySavedTracks = async (
+  session: AuthSession,
+  limit = 20,
+  offset?: number,
+  market?: string
+) => await getMySavedItems(session, "tracks", limit, offset, market);
 
 /**
  * @description get user's top ARTISTS or TRACKS
@@ -112,3 +146,16 @@ export const getUserById = async ({
 //     }),
 //   }).then((res) => res.json());
 // }
+export const getNewReleases = async (
+  session: AuthSession,
+  limit = 20,
+  offset?: number
+) => {
+  const searchParams = new URLSearchParams({ limit: limit.toString() });
+  if (offset) searchParams.set("offset", offset.toString());
+  return customFetch(
+    `${SPOTIFY_URL_BASE}/v1/browse/new-releases`,
+    session,
+    searchParams
+  );
+};
