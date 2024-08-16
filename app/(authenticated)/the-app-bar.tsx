738417b5d7412318cda6d3fac17c4417ab00d200
Eric <ericscottrichards@gmail.com>
2024-08-15 22:40:07 -0600

    profile menu

--- app/(authenticated)/the-app-bar.tsx
+++ app/(authenticated)/the-app-bar.tsx
@@ -1,29 +1,55 @@
 "use client";
-
-import { AuthSession, AuthUser } from "@/types/types";
+import { AuthUser } from "@/types/types";
 import {
   AppBar,
   Avatar,
   Button,
   IconButton,
   Link,
+  Menu,
+  MenuItem,
   Typography,
 } from "@mui/material";
-import { redirect } from "next/navigation";
-import { useSession } from "next-auth/react";
-import MenuIcon from "@mui/icons-material/Menu";
+import { redirect, useRouter } from "next/navigation";
+import { signOut, useSession } from "next-auth/react";
 import defaultProfileImage from "@/public/images/profile.png";
-import { useEffect, useLayoutEffect, useState } from "react";
-import { AppSearchBar } from "./app-search-bar";
+import { useLayoutEffect, useState } from "react";
 
 export default function TheAppBar() {
   const [profileImageSrc, setProfileImageSrc] = useState(
     defaultProfileImage.src
   );
+
   const session = useSession();
   if (!session) {
     redirect("/login");
   }
+
+  const router = useRouter();
+
+  const [profileMenuAnchorEl, setProfileMenuAnchorEl] =
+    useState<null | HTMLElement>(null);
+
+  const profileMenuOpen = Boolean(profileMenuAnchorEl);
+
+  const handleProfileImgClick = (
+    event: React.MouseEvent<HTMLButtonElement>
+  ) => {
+    setProfileMenuAnchorEl(event.currentTarget);
+  };
+
+  const handleProfileMenuClose = () => {
+    setProfileMenuAnchorEl(null);
+  };
+
+  const navToProfile = () => {
+    router.push("profile");
+    setProfileMenuAnchorEl(null);
+  };
+
+  const logout = () => {
+    signOut();
+  };
 
   const { data } = session;
 
@@ -51,13 +77,10 @@ export default function TheAppBar() {
       </Typography>
 
       <Link href="/">
-        <Button variant="text">Home</Button>
-      </Link>
-      <Link href="/profile">
-        <Button variant="contained">Profile</Button>
+        <Button variant="contained">Home</Button>
       </Link>
       <Link href="/users/99">
-        <Button variant="outlined">99</Button>
+        <Button variant="contained">99</Button>
       </Link>
       <Link href="/playlists">
         <Button variant="contained">Playlists</Button>
@@ -68,9 +91,21 @@ export default function TheAppBar() {
       <Link href="/top-tracks">
         <Button variant="contained">Top Tracks</Button>
       </Link>
-      <IconButton>
+      <IconButton onClick={handleProfileImgClick}>
         <Avatar src={profileImageSrc} />
       </IconButton>
+      <Menu
+        id="profile-menu"
+        anchorEl={profileMenuAnchorEl}
+        open={profileMenuOpen}
+        onClose={handleProfileMenuClose}
+        MenuListProps={{
+          "aria-labelledby": "basic-button",
+        }}
+      >
+        <MenuItem onClick={navToProfile}>Profile</MenuItem>
+        <MenuItem onClick={logout}>Logout</MenuItem>
+      </Menu>
     </AppBar>
   );
 }
